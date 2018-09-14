2c304b129876cefbcc14d0255f8b9444bb52ca2f
DailyDataMiner <benjaminmartin@theitguy.me>
2018-09-13 21:40:37 -0400

    v0.9 backend for RecApi Angular fullstack application.

---
+++ Backend/src/main/java/com/ex/controller/UserLoginController.java
@@ -0,0 +1,69 @@
+package com.ex.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ex.beans.UserLogin;
+import com.ex.services.UserLoginService;
+
+@RestController
+@RequestMapping("/login")
+@CrossOrigin(origins = "*")
+public class UserLoginController {
+	
+	@Autowired
+	private UserLoginService userLoginService;
+	
+	// Takes in UserLogin object (username, password)
+	@RequestMapping(value="/create", method=RequestMethod.POST,
+		consumes=MediaType.APPLICATION_JSON_VALUE,
+		produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserLogin> addUserLogin(@RequestBody UserLogin a) {
+		try {
+			a = userLoginService.addUserLogin(a);
+			if(a == null) {
+				return new ResponseEntity<UserLogin>(HttpStatus.CONFLICT);
+			}
+			else {
+				return new ResponseEntity<UserLogin>(a, HttpStatus.CREATED);
+			}
+		} catch (NullPointerException e) {
+			return null;
+		}
+	}
+	
+	// Takes in UserLogin object (username, password) and returns it if found and null if not found
+	@RequestMapping(value="/validate", method=RequestMethod.POST,
+		consumes=MediaType.APPLICATION_JSON_VALUE,
+		produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserLogin> getUserLogin(@RequestBody UserLogin a) {
+		try {
+		UserLogin result = userLoginService.getByUsername(a.getUsername());
+		if (result.getPassword().equals(a.getPassword())) {
+			System.out.println(a);
+			return new ResponseEntity<UserLogin>(a, HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<UserLogin>(HttpStatus.NOT_FOUND);
+		}
+		} catch (NullPointerException e) {
+			return null;
+		}
+	}
+	
+	@RequestMapping(value="/all", method=RequestMethod.GET,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<UserLogin>> getAll() {
+		List<UserLogin> list = userLoginService.getAll();
+		return new ResponseEntity<List<UserLogin>>(list, HttpStatus.OK);
+	}
+}
